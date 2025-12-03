b33bcfb701fcc5f67b19b03ca2102373fabc67b6
Ashton <ashtondsouza192@gmail.com>
2025-12-03 12:03:54 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Search } from "lucide-react";
+
 export default function App() {
   return (
-    <div className="bg-teal-400">
-      <div id="navbar" className="px-5 py-8 uppercase flex justify-between items-center"> {["Home", "Services", "ABOUT", "Contact", "faq"].map((item) => (<h1 className="font-bold">{item}</h1>))}
-       <div id='search' className="rounded-3xl bg-neutral-100 h-full p-1  w-[250px] flex items-center justify-end">
-    <h1>search</h1>
-    <div></div>
+    <div className="bg-teal-400 ">
+      <div id="navbar" className="px-5 py-10  uppercase flex justify-between items-center"> {["Home", "Services", "ABOUT", "Contact", "faq"].map((item) => (<h1 className="font-bold text-xl">{item}</h1>))}
+       <div className="flex gap-10 mr-20 ">
+         <div id='search' className="rounded-3xl shadow-xl bg-neutral-100 h-full p-1  w-[300px] flex items-center justify-end"><h1>search</h1> <div className="bg-green-700 rounded-full p-2 ml-2" ><Search/></div>
+         </div>
+         <button id='signupbutton' className="uppercase  bg-green-900 text-white px-6 py-2 rounded-3xl">
+          sign up
+          </button>
+               </div>
        </div>
-      </div>
     </div>
   );  
 }
