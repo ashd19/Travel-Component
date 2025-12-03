94e51e182a78226fd4644d867f755bb1a7436fe3
Ashton <ashtondsouza192@gmail.com>
2025-12-03 12:48:59 +0530

    recent changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,13 @@ export default function App() {
         <div className="flex gap-10 mr-20 ">
           <div
             id="search"
-            className="rounded-3xl shadow-xl bg-neutral-100 h-full p-1  w-[300px] flex items-center justify-end"
+            className="rounded-3xl shadow-xl bg-neutral-100 h-full py-1 px-2  w-[300px] flex items-center justify-end"
           >
-            <input type="text" placeholder="SEARCH"  onFocus={'border border-white'}/>
+            <input
+              type="text"
+              placeholder="SEARCH"
+              onFocus={"border border-white"}
+            />
             <div className="bg-green-700 rounded-full p-2 ml-2">
               <Search />
             </div>
@@ -35,20 +39,31 @@ export default function App() {
           </button>
         </div>
       </div>
-      <div id="hero" className="flex flex-col  ml-5 justify-start gap-5">
+      <div id="hero" className="flex flex-col  ml-5 items-start justify-start gap-5">
         <div className="text-[6vw] leading-none text-white font-bold">
           <h1>
             TRAVEL <br />
             THE WORLD
           </h1>
         </div>
-        <div className="ml-5 text-6xl leading-18 text-white font-light tracking-tighter italic">
+        <div className=" text-6xl leading-18 text-white font-light tracking-tighter italic">
           <h1>
             "Discover new destinations,
             <br />
             unforgettable experiences <br />& endless adventures"
           </h1>
         </div>
+        <div>
+          <p className="text-white leading-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, quod <br />
+            atque veniam recusandae autem maiores facilis natus sequi optio sed <br />
+            molestias ut quo sunt reiciendis praesentium excepturi culpa! <br />
+            Tempora, voluptas!
+          </p>
+        </div>
+        <button className="bg-white text-teal-800 rounded-3xl w-[130px]  uppercase px-2 font-bold py-2">
+BOOK NOW 
+        </button>
       </div>
     </div>
   );
