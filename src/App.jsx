ae3add1d75c0c4fe105389534b8f28798a43576f
Ashton <ashtondsouza192@gmail.com>
2025-12-21 14:18:16 +0530

    Latest working version

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function App() {
       }}
     >
       <div
-        className="absolute inset-0 pointer-events-none"
+        className="absolute inset-0 pointer-events-none z-0"
         style={{
           backgroundImage:
             "linear-gradient(to right,oklch(70.4% 0.14 182.503),oklch(77.7% 0.152 181.912),oklch(95.3% 0.051 180.801))",
@@ -22,13 +22,15 @@ export default function App() {
       ></div>
 
       {/* Background Clipped Shapes revealing original BG */}
-      {["bottom-0 left-0 -rotate-45", "top-0 right-10 -rotate-45"].map((pos, i) => (
+      {["bottom-0 left-0", "top-0 right-10"].map((pos, i) => (
         <div
           key={i}
-          className={`absolute ${pos} w-[690px] h-[267px] z-20`}
+          className={`absolute ${pos} w-[690px] h-[267px] z-5`}
           style={{
-            clipPath: "path('M1.86617 231.405C-5.41886 205.999 9.27103 179.497 34.6769 172.212L628.745 1.86618C654.151 -5.41884 680.652 9.27101 687.937 34.6769C695.222 60.0828 680.532 86.5841 655.126 93.8691L61.0584 264.215C35.6525 271.5 9.1512 256.81 1.86617 231.405Z')",
-            WebkitClipPath: "path('M1.86617 231.405C-5.41886 205.999 9.27103 179.497 34.6769 172.212L628.745 1.86618C654.151 -5.41884 680.652 9.27101 687.937 34.6769C695.222 60.0828 680.532 86.5841 655.126 93.8691L61.0584 264.215C35.6525 271.5 9.1512 256.81 1.86617 231.405Z')",
+            clipPath:
+              "path('M1.86617 231.405C-5.41886 205.999 9.27103 179.497 34.6769 172.212L628.745 1.86618C654.151 -5.41884 680.652 9.27101 687.937 34.6769C695.222 60.0828 680.532 86.5841 655.126 93.8691L61.0584 264.215C35.6525 271.5 9.1512 256.81 1.86617 231.405Z')",
+            WebkitClipPath:
+              "path('M1.86617 231.405C-5.41886 205.999 9.27103 179.497 34.6769 172.212L628.745 1.86618C654.151 -5.41884 680.652 9.27101 687.937 34.6769C695.222 60.0828 680.532 86.5841 655.126 93.8691L61.0584 264.215C35.6525 271.5 9.1512 256.81 1.86617 231.405Z')",
             backgroundImage: bgImage,
             backgroundSize: "cover",
             backgroundPosition: "center",
@@ -39,11 +41,13 @@ export default function App() {
 
       {/* Navbar */}
       <div className="relative px-25 py-10 inter-nav uppercase flex justify-between items-center text-black z-100">
-        {["Home", "Services", "ABOUT", "Contact", "faq"].map((item) => (
-          <h1 key={item} className="inter-nav  text-xl  cursor-pointer">
-            {item}
-          </h1>
-        ))}
+        <div className="flex gap-15">
+          {["Home", "Services", "ABOUT", "Contact", "faq"].map((item) => (
+            <h1 key={item} className="inter-nav text-xl cursor-pointer">
+              {item}
+            </h1>
+          ))}
+        </div>
         <div className="flex gap-10 mr-20">
           <div className="rounded-3xl shadow-xl  bg-neutral-100 py-1 px-2 w-[320px] flex items-center justify-end">
             <input
